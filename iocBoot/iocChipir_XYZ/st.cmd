4e784c690142b1dd0d0841fda8d50a8453a07d9c
Kathryn Baker <kathryn.baker@stfc.ac.uk>
2014-03-19 15:33:46 +0000

    Re-created the IOC to match the others in use in the remote viewer
    Former-commit-id: 01b26f74d4f967c9240c68afd56a9c1f21a3122a

--- iocBoot/iocChipir_XYZ/st.cmd
+++ iocBoot/iocChipir_XYZ/st.cmd
@@ -5,32 +5,35 @@
 
 < envPaths
 
-epicsEnvSet "IOCNAME" "$(P=$(MYPVPREFIX))CHIPIR_XYZ"
-epicsEnvSet "IOCSTATS_DB" "$(DEVIOCSTATS)/db/iocAdminSoft.db"
-
 cd ${TOP}
 
 ## Register all support components
 dbLoadDatabase "dbd/Chipir_XYZ.dbd"
 Chipir_XYZ_registerRecordDeviceDriver pdbbase
 
-# Turn on asynTraceFlow and asynTraceError for global trace, i.e. no connected asynUser.
-#asynSetTraceMask("", 0, 17)
+##ISIS## Run IOC initialisation 
+< $(IOCSTARTUP)/init.cmd
 
-## main args are:  portName, configSection, configFile, host, options (see lvDCOMConfigure() documentation in lvDCOMDriver.cpp)
-##
-## there are additional optional args to specify a DCOM ProgID for a compiled LabVIEW application 
-## and a different username + password for remote host if that is required 
-##
-## the "options" argument is a combination of the following flags (as per the #lvDCOMOptions enum in lvDCOMInterface.h)
-##    viWarnIfIdle=1, viStartIfIdle=2, viStopOnExitIfStarted=4, viAlwaysStopOnExit=8
+## Load record instances
+
+##ISIS## Load common DB records 
+< $(IOCSTARTUP)/dbload.cmd
+
+## Load our record instances
+#dbLoadRecords("db/xxx.db","user=kvlb23Host")
+epicsEnvSet "IOCNAME" "$(P=$(MYPVPREFIX))CHIPIR_XYZ"
 #lvDCOMConfigure("frontpanel", "frontpanel", "$(TOP)/Chipir_XYZApp/protocol/Chipir_XYZ.xml", "ndxchipir", 6, "", "spudulike", "reliablebeam")
-lvDCOMConfigure("frontpanel", "frontpanel", "$(TOP)/Chipir_XYZApp/protocol/Chipir_XYZ.xml", "", 6)
-
+lvDCOMConfigure("frontpanel", "frontpanel", "$(TOP)/Chipir_XYZApp/protocol/Chipir_XYZ.xml", "", 6, "", "")
 dbLoadRecords("$(TOP)/db/Chipir_XYZ.db","P=$(IOCNAME):")
-dbLoadRecords("$(IOCSTATS_DB)","IOC=$(IOCNAME)")
 
-#asynSetTraceMask("frontpanel",0,0xff)
-asynSetTraceIOMask("ex1",0,0x2)
+##ISIS## Stuff that needs to be done after all records are loaded but before iocInit is called 
+< $(IOCSTARTUP)/preiocinit.cmd
 
+cd ${TOP}/iocBoot/${IOC}
 iocInit
+
+## Start any sequence programs
+#seq sncxxx,"user=kvlb23Host"
+
+##ISIS## Stuff that needs to be done after iocInit is called e.g. sequence programs 
+< $(IOCSTARTUP)/postiocinit.cmd
